c7e3987ad79220ce8f5e9497adff09d386c5ef5b
Hammad-Ali-56 <97965719+Hammad-Ali-56@users.noreply.github.com>
2022-12-29 00:04:33 +0500

    The display function updated

--- finalcode.py
+++ finalcode.py
@@ -1,8 +1,13 @@
 from tkinter import *
 import pickle
 from tkinter.messagebox import askokcancel, showinfo, WARNING
+from tkinter import ttk
 from time import sleep
 from random import randint
+import pandas as pd
+import numpy
+import csv
+
 
 # IMPORTING FILES
 try:
@@ -22,11 +27,17 @@ except:
     pickle.dump(info, fileobj)  # dumping the info to the file
     fileobj.close()
 
+
 user_passes = info[0]
 amounts = info[1]
 creditCards = info[2]
 usernames = user_passes.keys()
 password = user_passes.values()
+
+dfa = [list(user_passes.keys()),list(user_passes.values()),list(amounts.values()),list(creditCards.values())]
+dfa = pd.DataFrame(dfa)
+
+admin_password = "1111"
 
 # Styling_variables
 bg = '#008080'
@@ -44,33 +55,6 @@ def update_file(info):
     pickle.dump(info, filesobj)  # dumping info into the file
     filesobj.close()
 
-
-# switching_between _pages
-def changepage():
-    global pagenum, root
-
-    for widget in root.winfo_children():
-        widget.destroy()
-    if pagenum == 1:
-        log_in_page(root)
-        pagenum = 3
-    elif pagenum == 2:
-        sign_up_page(root)
-        pagenum = 1
-    elif pagenum == 3:
-        user_account(root)
-        pagenum = 2
-
-
-def go_to_sign_up():
-    global root, pagenum
-
-    for widget in root.winfo_children():
-        widget.destroy()
-    sign_up_page(root)
-    pagenum = 1
-
-
 def on_enter(e):
     e.widget['foreground'] = fg
 
@@ -81,22 +65,27 @@ def on_leave(e):
 
 # Front_end
 def sign_up_page(root):
+    for widget in root.winfo_children():
+        widget.destroy()
     global img2
     page = Frame(root)
 
     Label(root, image=img2).place(x=600, y=0)
-    Label(root, text="WELCOME TO\nBANKING SYSTEM", justify=CENTER, font='Century 30 bold', bg=bg, fg='#17202A').place(
-        x=50, y=80)
+    Label(root, text="WELCOME TO BANKING\nMANAGEMENT SYSTEM", justify=CENTER, font='Century 30 bold', bg=bg, fg='#17202A').place(
+        x=25, y=80)
     Label(root, text="USERNAME", font=font1, bg=bg, fg=fg).place(x=22, y=226)
     Label(root, text="MASTER CARD NUMBER", font=font1, bg=bg, fg=fg).place(x=22, y=320)
     Label(root, text="PASSWORD", font=font1, bg=bg, fg=fg).place(x=22, y=410)
     Label(root, text="CONFIRM PASSWORD", font=font1, bg=bg, fg=fg).place(x=22, y=500)
     Label(root, text="Already have an account?", font='calibri 12', fg='#808B96', bg=bg).place(x=310, y=603)
+    Label(root, text="Sign in as admin", font='calibri 12', fg='#808B96', bg=bg).place(x=310, y=628)
     Button(root, text="SIGN-UP", font='rockwell 19', borderwidth=0, width="15", bg='#1F1A24', fg=bg, cursor='hand2',
            command=lambda: sign_up(en_username.get(), password.get(), pass_confirm.get(),
                                    credit_card_number.get())).place(x=30, y=600)
     Button(root, text="LOG-IN", font='calibri 13 underline', borderwidth=0, width="6", pady=0, padx=0, fg='#17202A',
-           bg=bg, cursor='hand2', command=changepage).place(x=483, y=600)
+           bg=bg, cursor='hand2', command=lambda: log_in_page(root)).place(x=483, y=600)
+    Button(root, text="ADMIN", font='calibri 13 underline', borderwidth=0, width="6", pady=0, padx=0, fg='#17202A',
+           bg=bg, cursor='hand2', command=lambda: admin_log_in_page(root)).place(x=483, y=625)
 
     en_username = StringVar()
     Label(root, text="___________________________________", font='arial 20', bg=bg, fg=fg).place(x=32, y=265)
@@ -120,6 +109,8 @@ def sign_up_page(root):
 
 
 def log_in_page(root):
+    for widget in root.winfo_children():
+        widget.destroy()
     global username, password, amounts, img2, bg, fg
     page = Frame(root)
 
@@ -131,7 +122,7 @@ def log_in_page(root):
     Button(root, text="LOG-IN", font='rockwell 19', borderwidth=0, width="20", bg='#1F1A24', fg=bg, cursor='hand2',
            command=lambda: log_in(en_username.get(), en_password.get(), en_credit.get())).place(x=175, y=530)
     back = Button(root, text="< Back to SIGN-UP", font='calibri 12', borderwidth=0, fg='#808B96', bg=bg, cursor='hand2',
-                  command=go_to_sign_up)
+                  command=lambda: sign_up_page(root))
     back.place(x=30, y=50)
     back.bind("<Enter>", on_enter)
     back.bind("<Leave>", on_leave)
@@ -149,13 +140,36 @@ def log_in_page(root):
     cred = Entry(root, textvariable=en_credit, font='arial 20', width=30, borderwidth=0, bg=bg, fg='#DAF7A6').place(
         x=35, y=350)
 
-
-def user_account(root):
-    global user, bg, fg
-    logout_img = PhotoImage(file="C:/Users/h/downloads/logout.png")
+def admin_log_in_page(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+    global adminpassword, img2, bg, fg
     page = Frame(root)
 
-    Label(root, text="WELCOME " + user.upper(), wraplength=431, justify=CENTER, font='calibri 36 bold underline',
+    Label(root, image=img2).place(x=600, y=0)
+    Label(root, text="ADMIN-LOG-IN", font='Century 30 bold underline', bg=bg, fg='#17202A').place(x=120, y=100)
+    Label(root, text="PASSWORD", font=font1, bg=bg, fg=fg).place(x=220, y=275)
+    Button(root, text="ADMIN-LOG-IN", font='rockwell 19', borderwidth=0, width="20", bg='#1F1A24', fg=bg, cursor='hand2',
+           command=lambda: admin_log_in(en_adminpassword.get())).place(x=125, y=480)
+    back = Button(root, text="< Back to SIGN-UP", font='calibri 12', borderwidth=0, fg='#808B96', bg=bg, cursor='hand2',
+                  command=lambda: sign_up_page(root))
+    back.place(x=30, y=50)
+    back.bind("<Enter>", on_enter)
+    back.bind("<Leave>", on_leave)
+
+    en_adminpassword = StringVar()
+    Label(root, text="___________________________________", font='arial 20', bg=bg, fg=fg).place(x=32, y=355)
+    pass_ = Entry(root, textvariable=en_adminpassword, show="●", font='arial 20', width=30, borderwidth=0, bg=bg,
+                  fg='#DAF7A6').place(x=35, y=350)
+
+def user_account(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+    global user, bg, fg
+    logout_img = PhotoImage(file="logout.png")
+    page = Frame(root)
+
+    Label(root, text="WELCOME!  " + user.upper(), wraplength=431, justify=CENTER, font='calibri 36 bold underline',
           bg=bg, fg='#1F1A24').place(x=290, y=100)
     Button(root, text="● Log out", font='calibri 14 bold underline', fg=bg, bg='#1F1A24', borderwidth=0, width="11",
            pady=7, command=logout).place(x=860, y=60)
@@ -169,6 +183,62 @@ def user_account(root):
            command=CurrentBal).place(x=100, y=250)
     Button(root, text="Transfer Money", font='arial 16', borderwidth=0, width="15", bg='#1F1A24', pady="15", fg=bg,
            command=transfer).place(x=700, y=250)
+
+def admin_account(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+    global  bg, fg
+    logout_img = PhotoImage(file="logout.png")
+    page = Frame(root)
+
+    Label(root, text="WELCOME ADMIN! " + user.upper(), wraplength=431, justify=CENTER, font='calibri 36 bold underline',
+          bg=bg, fg='#1F1A24').place(x=290, y=80)
+    Button(root, text="● Log out", font='calibri 14 bold underline', fg=bg, bg='#1F1A24', borderwidth=0, width="11",
+           pady=7, command=logout).place(x=860, y=60)
+    Button(root, text="● Log out", font='calibri 14 bold ', bg=bg, fg='#1F1A24', borderwidth=0, width="10",
+           command=logout).place(x=865, y=66)
+    Button(root, text="Display Information", font='arial 16', borderwidth=0, width="15", pady="15", bg='#1F1A24', fg=bg,
+           command=display_information).place(x=400, y=250)
+
+
+
+def display_information():
+    def openfile():
+        labels = ["Password","Current Balance","Credit Card Number"]
+        # df=pd.DataFrame(dfa,index =labels)
+        # df.to_csv("display.csv")
+        # filename = "display.csv"
+        # dfa = pd.read_csv(filename)
+        table.delete(*table.get_children())
+        table["columns"] = list(dfa.columns) # Changed
+        table["show"] = "headings"
+        table.delete(*table.get_children())
+        for column in table["columns"]:
+            table.column(column, width=70)
+            table.heading(column, text=column)
+
+        table_rows = dfa.to_numpy().tolist()
+        for row in table_rows:
+            table.insert("", "end", values=row)
+        # return None
+    for widget in root.winfo_children():
+        widget.destroy()
+    logout_img = PhotoImage(file="logout.png")
+    page = Frame(root)
+    global bg, fg
+    frame1=Frame(admin_account(root) )
+    frame1.place(x=75,y=200,height= 400,width= 900)
+    frame1.place(x=80,y=200,height= 400,width= 900)
+    table = ttk.Treeview(frame1)
+    table.place(relheight=1, relwidth=1)
+    scrolly = ttk.Scrollbar(frame1, orient=VERTICAL, command=table.yview)
+    scrollx = ttk.Scrollbar(frame1, orient=HORIZONTAL, command=table.xview)
+    table.configure(xscrollcommand=scrollx.set, yscrollcommand=scrolly.set)
+    scrollx.pack(fill=X, side=BOTTOM)
+    scrolly.pack(fill=Y, side=RIGHT)
+    openfile()
+
+
 
 
 def transfer():
@@ -211,7 +281,7 @@ def sign_up(username, password, con_pass, card_number):
         Label(root, text="Weak Password! Must be larger than 8 characters", bg=bg, fg='red', font='calibri').place(x=30,
                                                                                                                    y=480)
         flag = False
-    if not (card_number.startswith('552233')) or len(card_number) != 16 or (card_number in list(creditCards.values())):
+    if not (card_number.startswith('551155')) or len(card_number) != 16 or (card_number in list(creditCards.values())):
         Label(root, text="Invalid credit card number!", bg=bg, fg='red', font='calibri').place(x=30, y=390)
         flag = False
 
@@ -238,11 +308,11 @@ def log_in(username, password, card_number):
 
         if not (user_passes[username] == password) or (len(password)) == 0:
             Label(root, text="Password Incorrect", bg=bg, fg='red', font='calibri').place(x=30, y=495)
-        if not (creditCards[username] == card_number) or not (credit_card.startswith('552233')) or (card_number == 0):
+        if not (creditCards[username] == card_number) or not (credit_card.startswith('551155')) or (card_number == 0):
             Label(root, text="Incorrect Master Card Number", bg=bg, fg='red', font='calibri').place(x=30, y=395)
         if (username in usernames) and (user_passes[username] == password) and (creditCards[username] == card_number):
             user += username
-            changepage()
+            user_account(root)
     except:
         showinfo(
             title='Credentials error',
@@ -250,6 +320,19 @@ def log_in(username, password, card_number):
             icon=WARNING,
         )
 
+def admin_log_in(adminpassword):
+    global admin_password, root, bg, fg
+    try:
+        if not (adminpassword == admin_password) or (len(password)) == 0:
+            Label(root, text="Password Incorrect", bg=bg, fg='red', font='calibri').place(x=30, y=395)
+        if (adminpassword == admin_password):
+            admin_account(root)
+    except:
+        showinfo(
+            title='Credentials error',
+            message='Make sure creditentials places are not empty!',
+            icon=WARNING,
+        )
 
 def CurrentBal():
     global amounts, user, bg, fg
@@ -265,15 +348,20 @@ def withdrawal_update(event, money):
     global amounts, info, bg, fg
 
     money = int(money)
-    money = abs(money)
-    remaining_m = amounts[user] - money
+    if money > int(amounts[user]):
+        showinfo(
+            title="Withdrawal of money",
+            message="As your balance is insufficient.\n" "$" + str(money) + " are not withdrawed successfully from your account"
+        )
+    else:
+        remaining_m = amounts[user] - money
+        amounts[user] = remaining_m
+        showinfo(
+            title="Withdrawal of money",
+            message="$" + str(money) + " are withdrawed successfully from your account\nRemaining Balance: $" + str(
+                remaining_m)
+        )
 
-    showinfo(
-        title="Withdrawal of money",
-        message="$" + str(money) + " are withdrawed successfully from your account\nRemaining Balance: $" + str(
-            remaining_m)
-    )
-    amounts[user] = remaining_m
     info[1] = amounts
 
     update_file(info)
@@ -337,7 +425,7 @@ def transfer_confirm(money, card_num):
     if not (card_num in card_numbers):
         Label(root, text="No user with this master card number found!" + "\t" * 10, bg=bg, fg='red',
               font='calibri').place(x=340, y=490)
-    if not (card_num.startswith('552233')) or len(card_num) != 16:
+    if not (card_num.startswith('551155')) or len(card_num) != 16:
         Label(root, text="Invalid credit card number!" + "\t" * 10, bg=bg, fg='red', font='calibri').place(x=340, y=490)
     if money <= 50:
         showinfo(
@@ -360,16 +448,15 @@ def transfer_confirm(money, card_num):
         try:
             to_user = card_numbers.index(card_num)
             reciever = users[to_user]
+            showinfo(
+                title='One-time Password',
+                message='Your OTP is:' + otp_ans
+            )
+            otp_pass.bind('<Return>', lambda event: transfer_ok('<Return>', otp.get(), otp_ans, money, reciever))
         except:
             Label(root, text="No user with this master card number found!" + "\t" * 10, bg=bg, fg='red').place(x=340,
                                                                                                                y=490)
         sleep(1)
-        showinfo(
-            title='One-time Password',
-            message='Your OTP is:' + otp_ans
-        )
-        otp_pass.bind('<Return>', lambda event: transfer_ok('<Return>', otp.get(), otp_ans, money, reciever))
-
 
 def transfer_ok(event, en_otp, otp, money, reciever):
     global amounts, user, bg, fg
@@ -396,7 +483,7 @@ def logout():
             title='Logout status',
             message='Successfully logout')
         user = ""  # username_string_empties_after_one_user_logout
-        changepage()
+        sign_up_page(root)
 
 
 #######################################################-MAIN PROGRAM-############################################################################
@@ -406,13 +493,13 @@ pagenum = 1  # initializing_page_number,later_used_for_switching_between_differe
 root = Tk()  # creating_window
 
 # importing images
-img = PhotoImage(file="Bank.png")
+img = PhotoImage(file="bank.png")
 img2 = PhotoImage(file="logo.png")
 
 # editing_window
-root.title("Banking Management System")  # title_of_window
+root.title("Group 3 Banking System")  # title_of_window
 root.geometry("1070x700")
-# root.resizable(0,0)
+root.resizable(0,0)
 root.config(bg=bg)
 root.iconphoto(False, img)
 Label(root, image=img2).place(x=600, y=0)
